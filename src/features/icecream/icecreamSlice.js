bf4d7806d0588a823f024f002b7e2fbeff7e221c
TommyOliveir <90339122+TommyOliveir@users.noreply.github.com>
2023-11-17 16:28:05 +0100

    update

--- src/features/icecream/icecreamSlice.js
+++ src/features/icecream/icecreamSlice.js
@@ -6,27 +6,29 @@ const initialState = {
 };
 
 const icecreamSlice = createSlice({
-  name: "icecream",
+  name: 'icecream',
   initialState,
   reducers: {
-    ordered: (state) => {
-      state.numOfIcecreams--;
+    ordered: state => {
+      state.numOfIcecreams--
     },
     restocked: (state, action) => {
-      state.numOfIcecreams += action.payload;
-    },
+      state.numOfIcecreams += action.payload
+    }
   },
-//   extraReducers; {
-//     ['cake/ordered']: (state) => {
-//         state.numOfIcecreams--
-//     },
-//   },
-  extraReducers: (builder) => {
-    builder.addCase(cakeOrdered, (state) => {
-      state.numOfIcecreams--;
-    });
+  //   extraReducers; {
+  //     ['cake/ordered']: (state) => {
+  //         state.numOfIcecreams--
+  //     },
+  //   },
+
+  //builder first argument is the action type, second is a function with state and payload argument in this case we only put state
+  extraReducers: builder => {
+    builder.addCase(cakeOrdered, state => {
+      state.numOfIcecreams--
+    })
   }
-});
+})
 
 export default icecreamSlice.reducer;
 export const { ordered, restocked } = icecreamSlice.actions
